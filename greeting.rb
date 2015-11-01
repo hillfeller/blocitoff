e2080141ee1e876dea5ca5bcc66b2d9bb5d964e4
Hillary <hillfeller@gmail.com>
2015-11-01 16:20:51 -0500

    Everything is changed

---
+++ greeting.rb
@@ -0,0 +1,41 @@
+def greeting
+    ARGV.each do |hello, name|
+        puts "#{hello} #{name}!"
+    end
+end
+puts greeting
+
+
+
+def greeting
+    
+ARGV.each do |arg2|
+    ARGV.each do |arg1|
+        puts "#{arg1} #{arg2}"
+    end
+end
+end
+
+
+hello = arg1 name = arg2
+
+    def greeting
+        for hello in greeting do
+            name = ARGV
+            ARGV.each do |name|
+                puts "#{hello} #{name}"
+            end
+    end
+end
+greeting
+
+
+
+ANSWER
+
+greeting = ARGV.shift
+    ARGV.each do |name|
+        puts "#{greeting} #{name}!"
+    end
+
+
