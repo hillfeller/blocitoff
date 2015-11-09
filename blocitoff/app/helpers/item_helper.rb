6b1514097441626659d7ca2c113a12856913d7e9
Hillary <hillfeller@gmail.com>
2015-11-09 16:33:45 -0500

    Assignment 9

--- blocitoff/app/helpers/item_helper.rb
+++ blocitoff/app/helpers/item_helper.rb
@@ -3,7 +3,7 @@ module ItemHelper
     from_time = item.created_at
     to_time = Time.now - 7.days
     output = ""
-    output += "<small> #{distance_of_time_in_words(from_time, to_time)} remaining until deletion, by #{item.user.name}</small>"
+    output += "<small> #{distance_of_time_in_words(item.created_at + 7.days, Time.now)} remaining until deletion, by #{item.user.name}</small>"
     output.html_safe
   end
 end
--- blocitoff/db/seeds.rb
+++ blocitoff/db/seeds.rb
@@ -1,24 +1,26 @@
 include RandomData
 
+10.times do
+  user = User.create!(
+
+    name:     RandomData.random_name,
+    email:    RandomData.random_email,
+    password: RandomData.random_sentence
+  )
+
+end
+
+users = User.all
 
 50.times do
   Item.create!(
-
+    user: users.sample,
     name:  RandomData.random_sentence
   )
 end
 items = Item.all
 
-10.times do
-  user = User.create!(
 
-  name:     RandomData.random_name,
-  email:    RandomData.random_email,
-  password: RandomData.random_sentence
-  )
-end
-
-users = User.all
 
 puts "Seed finished"
 puts "#{Item.count} items created."
